bda4dc78a8bf223abb63c349c75cd17d897530f1
Trung Ngo <ndtrung4419@gmail.com>
2016-10-10 16:51:01 +0300

    Change lunch menu format in DB to use week day as key

--- forwarder/main.py
+++ forwarder/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-# fetch lunch menu from ihana and store into Firebase
+"""
+Fetch lunch menu from ihana and store into Firebase.
+"""
 
 import requests
 
@@ -21,13 +23,41 @@ def lunch_menu_url(campus='vanhamaantie'):
     return template.format(campus, 'sodexo')
 
 
+def number_to_day_of_week(num):
+    return ["", "mon", "tue", "wed", "thu", "fri", "sat", "sun"][num]
+
+
+def process_week_menu(week_menu):
+    # week_menu is a object with string keys representing the date ("1" for monday)
+    # But I don't like using number for day of week (not easy to understand at
+    # first sight and open to interpretation: should 1 be monday or sunday?)
+    return {
+        number_to_day_of_week(int(day)): process_lunch_menu(menu)
+        for day, menu in week_menu.items()
+    }
+
+
+def test_process_week_menu():
+    result = process_week_menu({
+        "1": ["1 / 2"],
+        "2": ["3 / 4"],
+    })
+
+    expected = {
+        "mon": [{"descriptionFi": "1", "descriptionEn": "2"}],
+        "tue": [{"descriptionFi": "3", "descriptionEn": "4"}],
+    }
+
+    assert expected == result
+
+
 def process_lunch_menu(menu):
     # the data returned by ihana is of the form
     # ["Finnish text / English text", "Finnish text / English text"]
     # we want to convert it to the form
     # [{descriptionFi: "Finnish text", descriptionEn: "English text"}]
     # other fields (like lunch type, price, etc. might be added later)
-    return map(process_lunch_menu_item, menu)
+    return [process_lunch_menu_item(item) for item in menu]
 
 
 def process_lunch_menu_item(item):
@@ -39,11 +69,6 @@ def test_process_lunch_menu_item():
     assert process_lunch_menu_item("1 / 2") == {"descriptionFi": "1", "descriptionEn": "2"}
     assert process_lunch_menu_item("1/2") == {"descriptionFi": "1", "descriptionEn": "2"}
     assert process_lunch_menu_item("1/ 2") == {"descriptionFi": "1", "descriptionEn": "2"}
-
-
-def process_week_menu(week_menu):
-    # week_menu is a object with string keys representing the date (1 for monday)
-    return map(process_lunch_menu, week_menu.values())
 
 
 def debug(value):
